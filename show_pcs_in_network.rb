9312eecbaf9f5eb1a74270cdc087d50cc092e15a
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-11-01 20:22:37 -0200

    Add show_pcs_in_network.rb

---
+++ show_pcs_in_network.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+if ARGV.count < 1
+  puts "Usage: sudo -E show_pcs_in_network.rb <network_type>"
+  puts "<network_type> exemple: eth0, lo, wlan0"
+  abort
+end
+
+network_type = ARGV[0]
+
+begin
+  user_ip = `sudo ifconfig`.split("\n\n").select{|line| line.start_with?(network_type)}.first.split("\n").select{|line| line.strip.start_with?("inet addr:")}.first.strip.split[1].split(':')[1]
+rescue
+  puts "Use a valid network_type"
+  puts ""
+  puts "Usage: sudo -E show_pcs_in_network.rb <network_type>"
+  puts "<network_type> exemple: eth0, lo, wlan0"
+  abort
+end
+
+ip_split = user_ip.split('.')
+ip_mask = "#{ip_split[0]}.#{ip_split[1]}.#{ip_split[2]}"
+
+puts(`sudo nmap -sU --script nbstat.nse -p137 #{ip_mask}.1-254`)
