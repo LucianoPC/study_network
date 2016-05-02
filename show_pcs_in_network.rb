c8f0b70b3149a1327f09a071429a478e908912c7
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-02 15:11:59 -0300

    Update script for debian sid

--- show_pcs_in_network.rb
+++ show_pcs_in_network.rb
@@ -9,8 +9,9 @@ end
 network_type = ARGV[0]
 
 begin
-  user_ip = `sudo ifconfig`.split("\n\n").select{|line| line.start_with?(network_type)}.first.split("\n").select{|line| line.strip.start_with?("inet addr:")}.first.strip.split[1].split(':')[1]
-rescue
+  user_ip = ic = `sudo ifconfig`.split("\n\n").select{|line| line.start_with?("wlan0")}.first.split("\n").select{|line| line.strip.start_with?("inet")}.first.strip.split[1].strip
+rescue => error
+  puts error
   puts "Use a valid network_type"
   puts ""
   puts "Usage: sudo -E show_pcs_in_network.rb <network_type>"
