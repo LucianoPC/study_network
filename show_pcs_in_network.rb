1a05bc6497bc311124bfdfe1528c76a5a4a0e5e7
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2018-04-12 13:11:56 +0000

    Fix script show_pcs_in_network

--- show_pcs_in_network.rb
+++ show_pcs_in_network.rb
@@ -9,7 +9,7 @@ end
 network_type = ARGV[0]
 
 begin
-  user_ip = ic = `sudo ifconfig`.split("\n\n").select{|line| line.start_with?("wlan0")}.first.split("\n").select{|line| line.strip.start_with?("inet")}.first.strip.split[1].strip
+  user_ip = ic = `sudo ifconfig`.split("\n\n").select{|line| line.start_with?(network_type)}.first.split("\n").select{|line| line.strip.start_with?("inet")}.first.strip.split[1].strip
 rescue => error
   puts error
   puts "Use a valid network_type"
